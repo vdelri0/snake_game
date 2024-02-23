f5bccc9ac2fe6db229623a74105459bbe2f30cf6
victorprens <vprens12@gmail.com>
2024-02-23 09:51:36 -0500

    Snake Game initial commit.

---
+++ main.py
@@ -0,0 +1,12 @@
+"""_summary_
++ First 3 Steps:
+- Create the body of the snake
+- Move the snake
+- Control the snake
+
++ Second 4 steps:
+- Detect collision with food
+- Create a scoreboard
+- Detect collision with a wall (The game should end)
+- Detect collision with tail (The game should end)
+"""
