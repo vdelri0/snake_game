27dd642b03aad9af058b67597cf1941c1c190ea3
victorprens <vprens12@gmail.com>
2024-02-28 14:24:40 -0500

    Snake Game First 3 steps completed.

--- main.py
+++ main.py
@@ -10,68 +10,40 @@
 - Detect collision with a wall (The game should end)
 - Detect collision with tail (The game should end)
 """
-
+from snake import Snake
 from turtle import Screen, Turtle
-from move import move_forwards, move_backwards, turn_left, turn_right, clear
 
 import time
 
-screen = Screen()
-snake = []
-speed = 1
-angles = [90, 180, 270, 360]
+ON = True
+SPEED = 1
+SLEEP = 0.1 # How fast the snake is going to move
 
+class main():
+    def __init__(self):
+        self.snake = Snake()
+        self.screen = Screen()
+        self.screen.title("Snake Game")
+        self.screen.setup(width=600, height=600)
+        self.screen.bgcolor("black")
+        self.screen.tracer(0)
+        self.screen.listen()
+        self.screen.onkey(self.snake.up, "w")
+        self.screen.onkey(self.snake.down, "s")
+        self.screen.onkey(self.snake.left, "a")
+        self.screen.onkey(self.snake.right, "d")
 
-def setup():
-    screen.title("Snake Game")
-    screen.setup(width=600, height=600)
-    screen.bgcolor("black")
-    # screen.listen()
-    screen.tracer(0)
+        while ON:
+            self.screen.update()
+            time.sleep(SLEEP)
+            self.snake.move()
+            self.snake
+        self.screen.exitonclick()
 
-# 1st step
-def create_snake():
-    for index in range(3):
-        segment = Turtle("square")
-        segment.penup()
-        segment.resizemode("user")
-        segment.shapesize(0.5, 0.5)
-        segment.color("white")
-        # segment.speed(speed)
-        if snake:
-            # print(f"xcor: {xcor}", f"ycor: {ycor}")
-            segment.backward(10 * index)
-        snake.append(segment)
-
-# 2nd step
-def move_snake():
-    print(snake)
-    screen.onkey(key="w", fun=lambda snake=snake: move_forwards(snake))
-    screen.onkey(key="s", fun=lambda snake=snake: move_backwards(snake))
-    screen.onkey(key="a", fun=lambda snake=snake: turn_left(snake))
-    screen.onkey(key="d", fun=lambda snake=snake: turn_right(snake))
-    screen.onkey(key="c", fun=lambda snake=snake: clear(snake))
-
-
-setup()
-create_snake()
-# move_snake()
-
-on = True
-while on:
-    screen.update()
-    time.sleep(0.1)
-    for seg_index in range(len(snake) - 1, 0, -1):
-        new_x = snake[seg_index - 1].xcor()
-        new_y = snake[seg_index - 1].ycor()
-        snake[seg_index].goto(new_x, new_y)
-    snake[0].forward(10)
-    # segments[0]
-
+if __name__ == '__main__':
+    main = main()
 
 # print("\n")
 # print(snake_body)
 # for snake in snake_body:
 #     print(snake.xcor(), snake.ycor())
-
-screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 10
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake():
+    # 1st step
+    def __init__(self):
+        self.snake = []
+        for index in range(3):
+            segment = Turtle("square")
+            segment.penup()
+            segment.resizemode("user")
+            segment.shapesize(0.5, 0.5)
+            segment.color("white")
+            # segment.speed(speed)
+            if self.snake:
+                # print(f"xcor: {xcor}", f"ycor: {ycor}")
+                segment.backward(10 * index)
+            self.snake.append(segment)
+        self.head = self.snake[0]
+
+    # 2nd step
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def clear(self):
+        self.snake.clear()
+        self.snake.penup()
+        self.snake.home()
+        self.snake.pendown()
+
+    def move(self):
+        for seg_index in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[seg_index - 1].xcor()
+            new_y = self.snake[seg_index - 1].ycor()
+            self.snake[seg_index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+        # snake[0].left(90)
